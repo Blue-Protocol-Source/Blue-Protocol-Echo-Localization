1226f4b43578253edac1bcbc21f82c1ba1e4e8b2
Goomii <5684215+GoomiiV2@users.noreply.github.com>
2023-06-28 07:31:25 +0100

    Some dark theme stuff

--- FesteLOC/Form1.Designer.cs
+++ FesteLOC/Form1.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(20, 20, 20);
+            ClientSize = new Size(800, 450);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Feste.LOC - Blue Protocol Translator";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
--- FesteLOC/Form1.cs
+++ FesteLOC/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace FesteLOC
 {
     public partial class Form1 : Form
@@ -5,6 +7,12 @@ namespace FesteLOC
         public Form1()
         {
             InitializeComponent();
+            Theme.UseImmersiveDarkMode(Handle, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
